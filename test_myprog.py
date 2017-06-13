5b75b0c7f2c321e10ff051f402246a937e7315cc
davibootcamp <davi.ortega@protonmail.com>
2017-06-13 14:51:42 -0700

    testing failing build

--- test_myprog.py
+++ test_myprog.py
@@ -3,3 +3,7 @@ import myprog
 def testi_if_it_works():
     assert myprog.neg_c('EEE') == 3
     return True
+
+def testi_if_it_works_with_lower_case_letters():
+    assert myprog.neg_c('eee') == 3
+    return True
